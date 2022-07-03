7da7d2f76b6bbf80474ddd5f1f4d11c4e2d0dddb
fandipro <fprasetyo4@gmail.com>
2022-07-03 21:43:43 +0700

    add chat model

--- src/model/chat.js
+++ src/model/chat.js
@@ -1,6 +1,8 @@
+const { decodeBase64 } = require("bcryptjs");
 const pool = require("../config/db");
 
 module.exports = {
+  // diambil pada saat get all user
   detailChat: (sender, receiver) =>
     new Promise((resolve, reject) => {
       pool.query(
@@ -14,5 +16,60 @@ module.exports = {
           }
         }
       );
+    }),
+
+  findBy: (field, search) =>
+    new Promise((resolve, reject) => {
+      console.log(search);
+      pool.query(`SELECT * FROM chats WHERE ${field} = $1`, [search], (err, result) => {
+        if (!err) {
+          resolve(result);
+        } else {
+          reject(err);
+        }
+      });
+    }),
+
+  insertChat: (id, sender, receiver, type, message, isRead) =>
+    new Promise((resolve, reject) => {
+      pool.query(`INSERT INTO chats VALUES ($1, $2, $3, $4, $5, $6)`, [id, sender, receiver, type, message, isRead], (err, result) => {
+        if (!err) {
+          resolve(result);
+        } else {
+          reject(err);
+        }
+      });
+    }),
+
+  listChat: (sender, receiver) =>
+    new Promise((resolve, reject) => {
+      pool.query(
+        `SELECT chats.id, userSender.id AS sender_id, userReceiver.id AS receiver_id
+      userSender.name AS sender, userReceiver.name AS receiver, chats.chat,
+      userSender.avatar AS sender_Avatar, userReceiver.avatar AS receiver_avatar,
+      chats.created_at FROM chats
+      LEFT JOIN users AS userSender ON chat.sender = userSender.id
+      LEFT JOIN users AS userReceiver ON chat.receiver = userReceiver.id
+      WHERE (sender = '${sender}' AND receiver='${receiver}')
+      OR (sender='${receiver}' AND receiver='${sender}')
+      `,
+        (err, result) => {
+          if (!err) {
+            resolve(result);
+          } else {
+            reject(err);
+          }
+        }
+      );
+    }),
+  deleteChat: (id) =>
+    new Promise((resolve, reject) => {
+      pool.query("DELETE FROM chats WHERE id = $1", [id], (err, res) => {
+        if (!err) {
+          resolve(res);
+        } else {
+          reject(err);
+        }
+      });
     }),
 };
