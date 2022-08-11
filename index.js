143bb353f00e580ad8c94a16d16341176cbb35d5
fandipras7 <fandiprasetyopro@gmail.com>
2022-08-11 10:32:28 +0700

    menambahkan cors pada app.user pertama

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ const httpServer = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://master--precious-pika-9e595f.netlify.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(morgan("dev"));
 
 app.use(
@@ -46,7 +51,7 @@ app.all("*", (req, res, next) => {
 const io = new Server(httpServer, {
   cors: {
     origin: ["http://localhost:3000", "https://master--precious-pika-9e595f.netlify.app"],
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
+    methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
 
